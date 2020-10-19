10f52844366a120ec0e86e356dad7ef936c58fb6
ohnchen <ohmejona@gmail.com>
2020-10-19 22:13:59 +0200

    Preparing min, hours countdown

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import time 
 #import getpass
 
-def countdown():
+def sec_countdown():
     value = int(input("Set your timer/countdown for ... seconds!"))
     for i in range(0, value-1):
     	time.sleep(1)
@@ -11,5 +11,25 @@ def countdown():
     	print(str(value))
     time.sleep(1)
 
-countdown()
+def min_countdown():
+    pass
+
+def hours_countdown():
+    pass
+
+unit = input("Which unit do you wish to use? ")
+
+try:
+    if unit == "s":
+        sec_countdown()
+    elif unit == "min":
+        min_countdown()
+    elif unit == "h":
+        hours_countdown()
+    else:
+        print("Not a  valid unit!!")
+except:
+    print("Not an option!")
+    input("Press enter to try again...")
+
 print("Time is up!!")
