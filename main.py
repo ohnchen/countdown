bcfc49f05f6563a8c5ad23c8a13608f6d44e6943
ohnchen <ohmejona@gmail.com>
2020-10-19 18:41:31 +0200

    first Version - only seconds

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 
 import time 
+#import getpass
 
+def countdown():
+    value = int(input("Set your timer/countdown for ... seconds!"))
+    for i in range(0, value-1):
+    	time.sleep(1)
+    	value -= 1
+    	print(str(value))
+    time.sleep(1)
 
+countdown()
+print("Time is up!!")
